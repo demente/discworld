028d33c58a46811083e46838f59663b96bbd8265
demente <olga.agen@gmail.com>
2019-01-13 13:53:33 +0100

    owner added to the building

--- src/main/java/discworld/model/asset/Building.java
+++ src/main/java/discworld/model/asset/Building.java
@@ -1,5 +1,20 @@
 package discworld.model.asset;
 
+import discworld.model.player.Player;
+
 public class Building {
 
+    private Player owner;
+
+    public Building(Player owner) {
+        this.owner = owner;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
 }
